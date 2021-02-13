6f9883969ddd7e342a1b345518694d6fc74d8e64
Nick <nick@dawncreative.co.uk>
2021-02-13 11:47:56 +0000

    fix missing key props in App.js

--- src/api/getInitalCategories.js
+++ src/api/getInitalCategories.js
@@ -2,14 +2,15 @@ import api from '.';
 import getToken from './getToken';
 
 const getInitalCategories = async () => {
-    try {
-        const token = await getToken();
-        const headers = { Authorization: token };
-        const { data } = await api.get('browse/categories/', { headers });
-        return data.categories;
-    } catch (error) {
-        console.log({ error });
-    }
+  try {
+    const token = await getToken();
+    const headers = { Authorization: token };
+    const { data } = await api.get('browse/categories/', { headers });
+    console.dir(data);
+    return data.categories;
+  } catch (error) {
+    console.log({ error });
+  }
 };
 
 export default getInitalCategories;
